3a76feeefade5f513f7e9b0770e259e9abe56414
Nahuel Illanes <nahuel.illanes4@gmail.com>
2025-06-09 22:56:30 -0300

    Actualizacion 8

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log("HolaMundito")
+//console.log("HolaMundito");
+
+/*
+let precio = parseFloat("200pe");
+
+console.log(precio);
+
+let numeroA = parseInt(prompt("Num 1: "))
+let numeroB = parseInt(prompt("Num 2: "))
+            
+let resultado = numeroA + numeroB
+
+document.writeln(resultado)
+
+
+
+let nombre = "Nahu"
+let apellido = "Crack"
+
+let saludo = nombre + apellido
+
+console.log(saludo)*/
